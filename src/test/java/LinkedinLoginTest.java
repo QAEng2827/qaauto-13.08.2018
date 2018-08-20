60745be2dfa7a4fd806d7dd3dc0df84d8d927a4a
Inna Nak <inna.nakopyuk@gmail.com>
2018-08-20 14:11:14 +0300

    version by Mikola

--- src/test/java/LinkedinLoginTest.java
+++ src/test/java/LinkedinLoginTest.java
@@ -25,41 +25,22 @@ public class LinkedinLoginTest {
         Assert.assertEquals(driver.getCurrentUrl(), "https://www.linkedin.com/", "Login page URL is wrong.");
         Assert.assertEquals(driver.getTitle(), "LinkedIn: Log In or Sign Up", "Login page title is wrong.");
 
-        //проверяем есть ли кнопка Sing in
+        WebElement userEmailField = driver.findElement(By.xpath("//input[@id = 'login-email']"));
+        WebElement userPasswordField = driver.findElement(By.xpath("//input[@id = 'login-password']"));
+        WebElement singInButton = driver.findElement(By.xpath("//input[@id='login-submit']"));
 
+        Assert.assertTrue(singInButton.isDisplayed(), "signIn button is not displayed on Login page.");
 
-        Assert.assertTrue(driver.findElement(By.xpath("//input[@class='login submit-button']")).isDisplayed());
+        // String loginEmail = "qaeng2728@gmail.com",loginPsw = "chertopoloh2827";
+        userEmailField.sendKeys("qaeng2728@gmail.com" );
+        userPasswordField.sendKeys("chertopoloh2827");
+        singInButton.click();
 
+        Assert.assertEquals(driver.getCurrentUrl(), "https://www.linkedin.com/feed/", "Home page URL is wrong.");
+        Assert.assertEquals(driver.getTitle(), "LinkedIn", "Page title is wrong.");
 
-        //лишнее, но не знаю. как еще визуализировать результат
-        boolean resultSearshButton = driver.findElement(By.xpath("//input[@class='login submit-button']")).isDisplayed();
-
-        if (resultSearshButton == true) {
-
-            System.out.println("yes");
-        } else {
-            System.out.println("No");
-        }
-        ;
-        //проверяем есть ли  поля для ввода логина  и пароля
-
-        Assert.assertTrue(driver.findElement(By.xpath("//input[@class = 'login-email']")).isDisplayed());
-
-        Assert.assertTrue(driver.findElement(By.xpath("//input[@class = 'login-password']")).isDisplayed());
-
-        //вводим логин и пароль
-        String loginEmail = "qaeng2728@gmail.com",loginPsw = "chertopoloh2827";
-
-        driver.findElement(By.id("login-email")).sendKeys(loginEmail);
-        driver.findElement(By.id("login-password")).sendKeys(loginPsw);
-        driver.findElement(By.xpath("//input[@class='login submit-button']")).click();
-
-        //  проверяем, выполнен ли вход на Home page
-        System.out.println(driver.getCurrentUrl());
-
-             Assert.assertEquals(driver.getCurrentUrl(), "https://www.linkedin.com/feed/", "Login page URL is wrong.");
-        Assert.assertTrue(driver.findElement(By.xpath("//li/a[@class = 'nav-item__link nav-item__link--underline js-nav-item-link active']")).isDisplayed());
-        System.out.println(driver.getCurrentUrl());
+        WebElement profileNavItem = driver.findElement(By.xpath("//li[@id='profile-nav-item']"));
+        Assert.assertTrue(profileNavItem.isDisplayed(), "profileNavItem button is not displayed on Home Page");
 
 
     }
